d49407ae06299a5f1d327d24cf8a94d05802347a
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-04-11 09:26:01 +0900

    Fix Mermaid

--- postgresql/mcp.go
+++ postgresql/mcp.go
@@ -8,26 +8,6 @@ import (
 	server "github.com/mark3labs/mcp-go/server"
 )
 
-// func createPostgreSQLServer() *server.MCPServer {
-// 	// 環境変数からデータベースURLを取得
-// 	databaseURL := os.Getenv("POSTGRESQL_DATABASE_URL")
-// 	if databaseURL == "" {
-// 		fmt.Println("Error: POSTGRESQL_DATABASE_URL environment variable not set.")
-// 		os.Exit(1)
-// 	}
-
-// 	// MCPサーバーを作成
-// 	s := server.NewMCPServer(
-// 		"PostgreSQL Database Server",
-// 		version,
-// 		server.WithResourceCapabilities(true, true),
-// 		server.WithLogging(),
-// 	)
-// 	s = SetPostgreSQLQueryServer(databaseURL, s)
-
-// 	return s
-// }
-
 func createPostgreSQLServer() *server.MCPServer {
 	// 環境変数からデータベースURLを取得
 	databaseURL := os.Getenv("POSTGRESQL_DATABASE_URL")
